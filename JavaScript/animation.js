761bdf6c7ecf951755772347625ec9d715982dcc
Sachin Gunasekara <gunasekara.s.u.d.a@gmail.com>
2024-06-10 09:48:20 +0530

    add animations

--- JavaScript/animation.js
+++ JavaScript/animation.js
@@ -32,3 +32,21 @@ hiddenElementy.forEach((el) =>observery.observe(el))
 
 
 /*end of the animation code */
+
+
+const observerUp = new IntersectionObserver((entries) => {
+    entries.forEach((entrie) => {
+        console.log(entrie)
+        if(entrie.isIntersecting){
+            entrie.target.classList.add('showUp');
+        }else{
+            entrie.target.classList.remove('showUp');
+        }
+    });
+})
+
+const hiddenElementUp = document.querySelectorAll('.hiddenUp');
+hiddenElementUp.forEach((el) =>observerUp.observe(el))
+
+
+/*end of the animation code */
